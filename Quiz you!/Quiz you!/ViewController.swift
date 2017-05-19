4d4bab44dd3546a509c9a1afd34e12d25eb34e3e
RICHARD THANT WIN HTET <thantw@10-150-44-123.dhcp-m.deakin.edu.au>
2017-05-19 19:24:43 +1000

    Commit 1

---
+++ Quiz you!/Quiz you!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz you!
+//
+//  Created by RICHARD THANT WIN HTET on 19/05/2017.
+//  Copyright © 2017 RICHARD THANT WIN HTET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
